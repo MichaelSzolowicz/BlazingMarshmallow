66092ba179e9fd18c35372649ae523f748aee77f
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2023-10-11 21:30:57 -0700

    Created GrappleHook class stub.

--- BlazingMarshmallow/Assets/Scripts/Abilities/GrappleHook.cs
+++ BlazingMarshmallow/Assets/Scripts/Abilities/GrappleHook.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class GrappleHook : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Rigidbody rb;
+
+    protected GameObject attachedTo;
+    protected Vector3 attachPoint;
+    [SerializeField] protected float length = 1;
+
+    [SerializeField] protected GameObject cursor;
+    [SerializeField] protected float cursorSensitivity = 1;
+    protected GrappleInput input;
+
+    private void Start()
     {
-        
+        input = new GrappleInput();
+        input.Enable();
+
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        SwivelCursor();
+    }
+
+    /// <summary>
+    /// Translate the cursor represented by a canvas object.
+    /// </summary>
+    protected void SwivelCursor()
+    {
+
+    }
+
+    /// <summary>
+    /// Shoot a ray from cursor.
+    /// Start applying grapple force if it hit an object in the grapple layer.
+    /// </summary>
+    protected void Fire()
+    {
+
+    }
+
+    /// <summary>
+    /// Stop applying grapple force.
+    /// </summary>
+    protected void Release()
+    {
+
+    }
+
+    /// <summary>
+    /// Constrain the rigidbody as if a rope of length was tied to attachPoint.
+    /// </summary>
+    protected void ApplyGrappleForce()
+    {
+
     }
 }
