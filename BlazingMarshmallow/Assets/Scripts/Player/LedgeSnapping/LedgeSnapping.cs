f2336d8b586c05a56595e1d02777ca3559f7e536
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2023-10-25 16:19:38 -0700

    Interpolation implemented

--- BlazingMarshmallow/Assets/Scripts/Player/LedgeSnapping/LedgeSnapping.cs
+++ BlazingMarshmallow/Assets/Scripts/Player/LedgeSnapping/LedgeSnapping.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class LedgeSnapping : MonoBehaviour
@@ -10,14 +11,17 @@ public class LedgeSnapping : MonoBehaviour
     [SerializeField] protected float minSlopeGradation = 88;
     [SerializeField] protected float maxSnapHeight = 10;
     [SerializeField] protected float forwardProbeDistance = 1;
+    [SerializeField] protected float interpSpeed = 1;
+    [SerializeField] protected float characterHeight = 2;
 
     protected Vector3 initialVelocity;
+    protected bool isInterpolating = false;
 
     private bool hasCollided;
 
     private void Update()
     {
-        initialVelocity = rb.velocity;
+        //initialVelocity = rb.velocity;
     }
 
     void Start()
@@ -27,6 +31,8 @@ public class LedgeSnapping : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        initialVelocity = -collision.relativeVelocity;
+
         print("LS collision");
 
         //if(hasCollided) { return; }
@@ -57,24 +63,73 @@ public class LedgeSnapping : MonoBehaviour
 
         print("LS angle: " + angle);
 
-        if(angle > minSlopeGradation) { SnapUp(); }
+        if(angle > minSlopeGradation ) { SnapUp(); }
 
 
     }
 
     private void SnapUp()
     {
-        Vector3 end = transform.position + transform.forward * forwardProbeDistance;
-        Vector3 start = end + Vector3.up * maxSnapHeight;
+        Vector3 end = transform.position + (transform.forward * forwardProbeDistance) - (transform.up * characterHeight / 2);
+        Vector3 start = end + transform.up * maxSnapHeight;
+        //Vector3 end = start - transform.up * (maxSnapHeight + characterHeight / 2);
         RaycastHit hit;
 
         Debug.DrawLine(start, end, Color.green, 100);
 
-        if(Physics.Linecast(start, end, out hit))
+        if(!isInterpolating && Physics.Linecast(start, end, out hit))
         {
-            transform.position = hit.point + transform.up * 2f;
-
-            rb.velocity = initialVelocity;
+            //transform.position = hit.point + transform.up * 2f;
+            rb.velocity = Vector3.zero;
+            StartCoroutine(InterpCoroutine(hit.point + transform.up));
         }
+    }
+
+
+    private IEnumerator InterpCoroutine(Vector3 targetPos)
+    {
+        
+        isInterpolating = true;
+        CapsuleCollider capsule = GetComponent<CapsuleCollider>();
+
+        while(Vector3.Distance(transform.position, targetPos) > 1f)
+        {
+            rb.velocity = Vector3.zero;
+
+            Vector3 point1 = transform.position - (transform.up * (capsule.height / 2 - capsule.radius));
+            Vector3 point2 = point1 + (transform.up * (capsule.height - capsule.radius));
+            Vector3 delta = Vector3.Lerp(transform.position, targetPos, Time.fixedDeltaTime * interpSpeed) - transform.position;
+            RaycastHit hit;
+
+            Debug.DrawLine(point1, point2, Color.magenta, 100);
+            Debug.DrawLine(point1 + delta, point2 + delta, Color.magenta, 100);
+
+            if (Physics.CapsuleCast(point1, point2, capsule.radius, delta.normalized, out hit, delta.magnitude))
+            {
+                print("LS capsule hit");
+                /*
+                Vector3 direction;
+                float dist;
+                bool b = Physics.ComputePenetration(capsule, transform.position, transform.rotation, hit.collider, hit.collider.transform.position, hit.collider.transform.rotation, out direction, out dist);
+                if(b)
+                {
+
+                }
+                */
+
+                delta = Vector3.ProjectOnPlane(delta, hit.normal).normalized * delta.magnitude;
+            }
+
+            transform.position += delta;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        rb.velocity = initialVelocity;
+        isInterpolating = false;
+
+        print("LS interp end");
+
+        StopAllCoroutines();
     }
 }
