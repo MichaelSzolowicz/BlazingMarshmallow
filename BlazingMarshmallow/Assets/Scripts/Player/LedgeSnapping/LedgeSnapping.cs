e08c58d85e7f03495a90eda44b2e5a26b6ff3fd9
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2023-11-08 05:49:06 -0800

    Update LedgeSnapping.cs

--- BlazingMarshmallow/Assets/Scripts/Player/LedgeSnapping/LedgeSnapping.cs
+++ BlazingMarshmallow/Assets/Scripts/Player/LedgeSnapping/LedgeSnapping.cs
@@ -18,7 +18,11 @@ public class LedgeSnapping : MonoBehaviour
     protected Vector3 initialVelocity;
     protected bool isInterpolating = false;
     protected Vector3 lastFrameVelocity;
+    protected Vector3 lastPosition;
 
+    Vector3 previousCollisionImpulse = Vector3.zero;
+    public float dotThresh = .7f;
+    public float magThresh = 5f;
 
     private void Update()
     {
@@ -54,12 +58,19 @@ public class LedgeSnapping : MonoBehaviour
 
         Debug.DrawLine(avgPoint, avgPoint + avgNormal * 500, Color.blue, 100.0f);
 
+
+        print("LS Impulse: " + collision.impulse + ", mag:  " + collision.impulse.magnitude + ", dot: " + Vector3.Dot(previousCollisionImpulse.normalized, collision.impulse.normalized) + ", obj: " + collision.gameObject.name);
+
+        print("LS impulse dist: " + Vector3.Distance(lastPosition, transform.position));
+        
+
+
         //float dot = Vector3.Dot(avgNormal, Vector3.up);
         //float angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
 
         //float velocityResistance = Mathf.Abs(Vector3.Dot(avgNormal, lastFrameVelocity.normalized));
 
-        if (avgPoint.y > (transform.position.y - characterHeight / 2) + .01f)
+        if (avgPoint.y > (transform.position.y - characterHeight / 2))
         {
             print("LS avgNormal: " + avgNormal + ", rbVelocity: " + lastFrameVelocity + ", Obj: " + collision.gameObject.name);
             if (!isInterpolating)
@@ -69,6 +80,9 @@ public class LedgeSnapping : MonoBehaviour
             }
             SnapUp(); 
         }
+
+        previousCollisionImpulse = collision.impulse;
+        lastPosition = rb.position;
     }
 
     private void SnapUp()
@@ -81,7 +95,10 @@ public class LedgeSnapping : MonoBehaviour
 
         if(!isInterpolating && Physics.Linecast(start, end, out hit))
         {
-            rb.velocity = Vector3.zero;
+            Vector3 t = initialVelocity;
+            t.y = 0;
+            rb.velocity = t;
+
             StartCoroutine(InterpCoroutine(hit.point + transform.up * characterHeight / 2));
         }
     }
@@ -91,16 +108,17 @@ public class LedgeSnapping : MonoBehaviour
     { 
         isInterpolating = true;
         CapsuleCollider capsule = GetComponent<CapsuleCollider>();
+        capsule.enabled = false;
+        rb.useGravity = false;
+        targetPos += Vector3.up * .1f;
 
-        while(Vector3.Distance(transform.position, targetPos) > .5f)
+        while((targetPos.y) - transform.position.y >= .1f)
         {
             //print("LS interping...");
 
-            rb.velocity = Vector3.zero;
-
             Vector3 point1 = transform.position - (transform.up * (capsule.height / 2 - capsule.radius));
             Vector3 point2 = point1 + (transform.up * (capsule.height - capsule.radius));
-            Vector3 delta = Vector3.Slerp(transform.position, targetPos, Time.fixedDeltaTime * interpSpeed) - transform.position;
+            Vector3 delta = Vector3.LerpUnclamped(transform.position, targetPos, Time.fixedDeltaTime * interpSpeed) - transform.position;
             RaycastHit hit;
 
             Debug.DrawLine(point1, point2, Color.magenta, 100);
@@ -113,15 +131,20 @@ public class LedgeSnapping : MonoBehaviour
                 delta = Vector3.ProjectOnPlane(delta, hit.normal).normalized * delta.magnitude;
             }
 
+            delta.x = 0;
+            delta.z = 0;
             transform.position += delta;
 
             yield return new WaitForFixedUpdate();
         }
 
-        rb.velocity = initialVelocity;
-        isInterpolating = false;
+        rb.useGravity = true;
+        yield return new WaitForFixedUpdate();
 
-        //print("LS interp end. Velocity: " + initialVelocity);
+        capsule.enabled = true;
+        isInterpolating = false;
+        //rb.velocity = initialVelocity;
+        print("LS interp end. Velocity: " + initialVelocity);
     }
 
     public bool GetIsInterpolating()
