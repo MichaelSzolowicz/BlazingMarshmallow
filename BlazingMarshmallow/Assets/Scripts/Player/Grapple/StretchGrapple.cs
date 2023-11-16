ccf00404e5259c1f871def9233498ee84f8470e2
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2023-11-15 20:17:18 -0800

    Rotate and stretch functions

--- BlazingMarshmallow/Assets/Scripts/Player/Grapple/StretchGrapple.cs
+++ BlazingMarshmallow/Assets/Scripts/Player/Grapple/StretchGrapple.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class StretchGrapple : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 grapplePoint = Vector3.zero;
+    public Vector3 testOffset = Vector3.zero;
+
+    Vector3 baseScale = Vector3.one;
+
+    protected void Start()
     {
-        
+        baseScale = transform.localScale;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected void Update()
     {
-        
+ 
+
+        print("SR rotate head");
+        RotateHead();
+        Stretch();
+    }
+
+    protected void RotateHead()
+    {
+        grapplePoint = transform.position + testOffset;
+        print("SR rotate head");
+
+        Vector3 diff = (grapplePoint - transform.position).normalized;
+        float angle = Vector3.Angle(Vector3.up, diff);
+        Vector3 axis = Vector3.Cross(Vector3.up, diff).normalized;
+
+        Quaternion newRot = Quaternion.AngleAxis(angle, axis);
+
+        transform.rotation = newRot;
+    }
+
+    protected void Stretch()
+    {
+        Vector3 diff = (testOffset);
+        float length = diff.magnitude;
+
+        print("SR loc scale " + baseScale);
+
+        Vector3 scale = baseScale;
+        scale.y *= length;
+        transform.localScale = scale;   
     }
 }
